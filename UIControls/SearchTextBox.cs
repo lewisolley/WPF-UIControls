6322d6d6f753b11907bf96bff97ec1084ce3d6a1
lewisolley <lewisolley93@gmail.com>
2015-10-03 11:45:08 +0100

    Added properties to SearchTextBox

---
+++ UIControls/SearchTextBox.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UIControls
+{
+    public enum SearchMode
+    {
+        Instant,
+        Delayed,
+    }
+    public class SearchTextBox : TextBox
+    {
+        public static DependencyProperty LabelTextProperty =
+            DependencyProperty.Register(
+                "LabelText",
+                typeof(string),
+                typeof(SearchTextBox));
+
+        public static DependencyProperty LabelTextColorProperty =
+            DependencyProperty.Register(
+                "LabelTextColor",
+                typeof(Brush),
+                typeof(SearchTextBox));
+
+        public static DependencyProperty SearchModeProperty =
+            DependencyProperty.Register(
+                "SearchMode",
+                typeof(SearchMode),
+                typeof(SearchTextBox),
+                new PropertyMetadata(SearchMode.Instant));
+
+        private static DependencyPropertyKey HasTextPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                "HasText",
+                typeof(bool),
+                typeof(SearchTextBox),
+                new PropertyMetadata());
+
+        public static DependencyProperty HasTextProperty = HasTextPropertyKey.DependencyProperty;
+        static SearchTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SearchTextBox), new FrameworkPropertyMetadata(typeof(SearchTextBox)));
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            HasText = Text.Length != 0;
+        }
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        public Brush LabelTextColor
+        {
+            get { return (Brush)GetValue(LabelTextColorProperty); }
+            set { SetValue(LabelTextColorProperty, value); }
+        }
+
+        public SearchMode SearchMode
+        {
+            get { return (SearchMode)GetValue(SearchModeProperty); }
+            set { SetValue(SearchModeProperty, value); }
+        }
+
+        public bool HasText
+        {
+            get { return (bool)GetValue(HasTextProperty); }
+            private set { SetValue(HasTextPropertyKey, value); }
+        }
+    }
+}
